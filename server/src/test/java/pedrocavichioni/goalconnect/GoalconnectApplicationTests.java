afa5fab17b6732675ec24560022a6ca13d003692
Pedro <ph.02cavichioni@gmail.com>
2024-10-23 18:31:24 -0400

    feat: Adding backend initial structure

---
+++ server/src/test/java/pedrocavichioni/goalconnect/GoalconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package pedrocavichioni.goalconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoalconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
