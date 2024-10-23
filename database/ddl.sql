d485eb13dce93f44beca74bec10da77f2d4ca98c
Pedro <ph.02cavichioni@gmail.com>
2024-10-23 18:07:21 -0400

    feat: Adding rest of the states

--- database/ddl.sql
+++ database/ddl.sql
@@ -6,7 +6,7 @@ CREATE TABLE team(
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     state VARCHAR(2) NOT NULL CHECK (state IN( 'AC', 'AL', 'AP', 'AM', 'BA','CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG',
-    'PA', 'PB', 'PR', 'PE', 'PI', 'RJ')),
+    'PA', 'PB', 'PR', 'PE', 'PI', 'RJ','RN', 'RS', 'RO', 'RR','SC', 'SP', 'SE', 'TO')),
     photo_url VARCHAR(1024) NOT NULL,
     
     unique(name, state)
