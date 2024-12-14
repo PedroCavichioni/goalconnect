5ce31cc2126968e5cd9ade7dfdd15e1f22bf0ba0
Pedro Henrique Cavichioni de Oliveira <ph.02cavichioni@gmail.com>
2024-12-14 19:14:36 -0400

    feat: adding team response DTO

---
+++ server/src/main/java/pedrocavichioni/goalconnect/dto/team/TeamResponseDTO.java
@@ -0,0 +1,31 @@
+package pedrocavichioni.goalconnect.dto.team;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import pedrocavichioni.goalconnect.model.Team;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TeamResponseDTO {
+
+    private Long id;
+
+    private String name;
+
+    private String state;
+
+    private String photoUrl;
+
+    public TeamResponseDTO(Team team){
+      this(
+              team.getId(),
+              team.getName(),
+              team.getState(),
+              team.getPhotoUrl());
+    }
+}
