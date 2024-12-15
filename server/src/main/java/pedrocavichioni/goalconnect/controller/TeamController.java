07a9a9c029f22c3f36046ca62c91374b836ee8e1
Pedro Henrique Cavichioni de Oliveira <ph.02cavichioni@gmail.com>
2024-12-15 00:17:30 -0400

    Comitando arquivos não rastreados

---
+++ server/src/main/java/pedrocavichioni/goalconnect/controller/TeamController.java
@@ -0,0 +1,44 @@
+package pedrocavichioni.goalconnect.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pedrocavichioni.goalconnect.dto.team.TeamRequestDTO;
+import pedrocavichioni.goalconnect.dto.team.TeamResponseDTO;
+import pedrocavichioni.goalconnect.model.Team;
+import pedrocavichioni.goalconnect.service.TeamService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/team")
+public class TeamController {
+
+    @Autowired
+    private TeamService teamService;
+
+    @GetMapping("/all")
+    public List<Team> getAllTeams(){
+        return teamService.getAllTeams();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<TeamResponseDTO> getTeamById(@PathVariable("id") Long id){
+        return ResponseEntity.ok(teamService.getTeamById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Team> addTeam(@RequestBody TeamRequestDTO teamRequestDTO){
+        return ResponseEntity.ok(teamService.registerTeam(teamRequestDTO));
+    }
+
+    @PutMapping
+    public String updateTeam(@RequestBody Team team){
+        return "";
+    }
+
+    @DeleteMapping("{id}")
+    public String deleteTeam(@PathVariable("id") Long id){
+        return "";
+    }
+}
