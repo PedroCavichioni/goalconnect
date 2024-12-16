933a6adf1b74a9e6645ca91cc6f33176e4e65510
Pedro Henrique Cavichioni de Oliveira <ph.02cavichioni@gmail.com>
2024-12-15 21:12:20 -0400

    feat: adding MatchRequestDTO e MatchResponseDTO

---
+++ server/src/main/java/pedrocavichioni/goalconnect/dto/match/MatchRequestDTO.java
@@ -0,0 +1,30 @@
+package pedrocavichioni.goalconnect.dto.match;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MatchRequestDTO {
+
+    private Long id;
+
+    private Date date;
+
+    private Integer scoreTeamOne;
+
+    private Integer scoreTeamTwo;
+
+    private Long teamOneId;
+
+    private Long teamTwoId;
+
+    private Long supportedTeamId;
+
+}
